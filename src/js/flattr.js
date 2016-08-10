de9e69ab6587490d52b8d11f5295304e9d682704
Janet Lehmann <janet.lehmann@erdfisch.de>
2016-08-10 10:40:44 +0200

    js changes

--- src/js/flattr.js
+++ src/js/flattr.js
@@ -1,6 +1,19 @@
 /**
  * Created by janet on 10.08.16.
  */
+
+(function() {
+  var s = document.createElement('script');
+  var t = document.getElementsByTagName('script')[0];
+
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//api.flattr.com/js/0.6/load.js?'+
+    'mode=auto&uid=gargamel&language=sv_SE&category=text';
+
+  t.parentNode.insertBefore(s, t);
+})();
+
 window.onload = function() {
     FlattrLoader.render({
       'uid': 'flattr',
