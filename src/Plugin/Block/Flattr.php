da756bf76440554c8474d40de2fff2386da1c05e
Janet Lehmann <janet.lehmann@erdfisch.de>
2016-08-11 14:07:21 +0200

    remove some things from debugging

--- src/Plugin/Block/Flattr.php
+++ src/Plugin/Block/Flattr.php
@@ -43,54 +43,6 @@ class Flattr extends BlockBase {
       ),
     );
 
-    // Here is an example of how to put some markup into our block.
-    $build['hello'] = array(
-      '#type' => 'markup',
-      '#markup' => '<p><strong>' . 'hello world' . '</p></strong>',
-    );
-
-    // So that you can debug your href value, for example.
-    $build['href'] = array(
-      '#type' => 'markup',
-      '#markup' => '<p>' . $base_url . \Drupal::service('path.current')->getPath() . '</p>',
-    );
-
-    // Or your username value from the configuration.
-    $build['username'] = array(
-      '#type' => 'markup',
-      '#markup' => '<p>' . $this->configuration['username'] . '</p>',
-    );
-
-    // Debugging: make a copy of the button.
-    // Take display:none out so that it is visible.
-    // Take the javascript attached library out.
-    // The button works like this: when the javascript runs it replaces the
-    // link we have created.
-    // What we are left with is the link that is created by the javascript.
-    // That's why it works now even without the picture.
-    // Here we display a copy of the button so that you can see what the href
-    // is. Then go to the flattr developer documentation and see if you can
-    // work out what the reason for the error is.
-    $build['button_visible'] = array(
-      '#type' => 'inline_template',
-      // See: http://developers.flattr.net/button/
-      // HTML5 code example.
-      '#template' => '<a
-        data-flattr-uid="{{username}}"
-        href="{{href}}">this is a link we can see and inspect</a>',
-      '#context' => [
-        // The flatter account name.
-        'username' => $this->configuration['username'],
-        // The href should refer to the page which is being "flattered".
-        'href' => $base_url . \Drupal::service('path.current')->getPath(),
-      ],
-//      '#attached' => array(
-//        'library' => array(
-//          'flattr/flattr',
-//        ),
-//      ),
-    );
-
     return $build;
   }
 
